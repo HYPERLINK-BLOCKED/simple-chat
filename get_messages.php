5dd025b16b7551d8444c753780a753d41bc3d09e
HYPERLINK-BLOCKED <tkacenkosasa956@gmail.com>
2024-05-02 22:29:18 +0300

    Add files via upload

---
+++ get_messages.php
@@ -0,0 +1,9 @@
+<?php
+$file = 'messages.txt';
+if (file_exists($file)) {
+    $messages = file($file);
+    foreach ($messages as $message) {
+        echo $message . '<br>';
+    }
+}
+?>
---
+++ send_message.php
@@ -0,0 +1,9 @@
+<?php
+$data = json_decode(file_get_contents('php://input'), true);
+
+if (!empty($data['message'])) {
+    $message = $data['message'];
+    $file = 'messages.txt';
+    file_put_contents($file, date('Y-m-d H:i:s') . ': ' . $message . PHP_EOL, FILE_APPEND);
+}
+?>
