f968ebf3482ff0156b6ed97e525fbd8eda079c4d
HYPERLINK-BLOCKED <tkacenkosasa956@gmail.com>
2024-07-23 22:58:19 +0300

    Update get_messages.php

--- get_messages.php
+++ get_messages.php
@@ -1,9 +1,1 @@
-<?php
-$file = 'messages.txt';
-if (file_exists($file)) {
-    $messages = file($file);
-    foreach ($messages as $message) {
-        echo $message . '<br>';
-    }
-}
-?>
+
